499fa6b866c32cf076b0bc28da08946f7dc8664d
Devon Rudd <nukeguy5@MacBook-Pro-3.local>
2018-12-23 02:05:37 -0700

    Simplified DH Key Exchange

--- DH key exchange/client.py
+++ DH key exchange/client.py
@@ -4,11 +4,6 @@ import random
 
 priv_key = random.randint(100000, 999999)
 sock = socket.socket()
-ip = '127.0.0.1'
-port = 12345
-
-# Connect to local socket
-sock.connect(('127.0.0.1', port))
 
 def send_data(socket, data):
     socket.send(bytes("SENDING", 'utf-8'))
@@ -35,10 +30,25 @@ def create_pub_key(priv_key, prime, gen):
     c_pub_key = (gen**priv_key) % prime
     return c_pub_key
 
-def calc_shared_secret(socket, priv_key, prime):
+def calc_shared_secret(socket, priv_key):
+	# Connect to local socket
+    ip = '127.0.0.1'
+    port = 12345
+    socket.connect((ip, port))
+    print(f'Connected to {ip}:{port}')
+
+	# Receive initial values
+    print("Receiving intial values...")
+    prime = int(recv_data(sock))
+    gen = int(recv_data(sock))
+
     c_pub_key = create_pub_key(priv_key, prime, gen)
     send_data(sock, c_pub_key)  # send client public key
     s_pub_key = int(recv_data(sock))  # recv server public key
+
+    # Close connection
+    sock.close()
+
     shared_secret = (s_pub_key**priv_key) % prime  # mathimatical function
     return shared_secret
 
@@ -46,12 +56,8 @@ if __name__ == '__main__':
     print('-'*24)
     print(f'Private Key: {priv_key}')
     print('-'*24)
-
-    # Receive initial values
-    prime = int(recv_data(sock))
-    gen = int(recv_data(sock))
     
-    result = calc_shared_secret(sock, priv_key, prime)
+    result = calc_shared_secret(sock, priv_key)
+
     print('-'*24)
     print(f'Shared Key: {result}')
-    sock.close()
--- DH key exchange/server.py
+++ DH key exchange/server.py
@@ -39,7 +39,16 @@ def create_pub_key(priv_key, prime, gen):
     s_pub_key = gen**priv_key % prime
     return s_pub_key
 
-def calc_shared_secret(socket, priv_key, prime):
+def calc_shared_secret(socket, priv_key):
+    # Accept connection
+    conn, (ip, port) = socket.accept()
+    print(f'Connected to {ip}:{port}')
+
+    # Send initial values
+    print("Sending initial values...\n")
+    send_data(conn, prime)
+    send_data(conn, gen)
+
     s_pub_key = create_pub_key(priv_key, prime, gen)
     c_pub_key = int(recv_data(conn))  # recv client public key
     send_data(conn, s_pub_key)  # send server public key
@@ -53,11 +62,7 @@ if __name__ == '__main__':
     print(f'Generator: {gen}')
     print('-'*24)
 
-    # Send initial values
-    conn, (ip, port) = sock.accept()
-    send_data(conn, prime)
-    send_data(conn, gen)
+    result = calc_shared_secret(sock, priv_key)
 
-    result = calc_shared_secret(conn, priv_key, prime)
     print('-'*24)
     print(f'Shared Key: {result}')
